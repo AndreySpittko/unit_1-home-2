f06945df555a9f8913f14615c9880db944ad4ced
AndreySpittko <andrey891109@gmail.com>
2019-03-12 14:45:07 +0200

    7th commit. test collection in 'class Main' and override all methods in 'class Employee'

--- src/com/company/Employee.java
+++ src/com/company/Employee.java
@@ -1,5 +1,98 @@
 package com.company;
 
+import java.util.Date;
+
 public class Employee extends Person {
+
+    // Constructors
+    Employee() {
+        super();
+    }
+
+    Employee(String name, String surName, int age, String location) {
+        super(name, surName, age, location);
+    }
+
+    Employee(String name, String surName, int age, String location, int yearOfBirth, int monthOfBirth, int dayOfBirth) {
+        super(name, surName, age, location, yearOfBirth, monthOfBirth, dayOfBirth);
+    }
+
+    @Override
+    public String getName() {
+        return super.getName();
+    }
+
+    @Override
+    public String getSurName() {
+        return super.getSurName();
+    }
+
+    @Override
+    public int getAge() {
+        return super.getAge();
+    }
+
+    @Override
+    public String getLocation() {
+        return super.getLocation();
+    }
+
+    @Override
+    public Gender getGender() {
+        return super.getGender();
+    }
+
+    @Override
+    public Date getBirthDay() {
+        return super.getBirthDay();
+    }
+
+    @Override
+    public void setName(String name) {
+        super.setName(name);
+    }
+
+    @Override
+    public void setSurName(String surName) {
+        super.setSurName(surName);
+    }
+
+    @Override
+    public void setAge(int age) {
+        super.setAge(age);
+    }
+
+    @Override
+    public void setLocation(String location) {
+        super.setLocation(location);
+    }
+
+    @Override
+    public void setGender(Gender gender) {
+        super.setGender(gender);
+    }
+
+    @Override
+    public void setBirthDay(Date birthDay) {
+        super.setBirthDay(birthDay);
+    }
+
+    @Override
+    public void showFullInfo() {
+        String infoAboutPerson = String.format("Employee name:\t%1$s\t\tyour age - %2$d and birthDay is %3$tY-%3$tB-%3$td\n\t\tyour gender - %4$s\n\t\tyou from - %5$s\n", fullName(), getAge(), getBirthDay(), getGender(), getLocation());
+        System.out.println(infoAboutPerson);
+    }
+
+    @Override
+    public void showShortInfo() {
+        String showInfoPerson = String.format("Employee name:\t%1$s\t\tyour age - %2$d\n\t\tyou from - %3$s", fullName(), getAge(), getLocation());
+        System.out.println(showInfoPerson);
+    }
+
+    @Override
+    public String fullName() {
+        return super.fullName();
+    }
+
 
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,8 @@ package com.company;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
@@ -23,6 +25,7 @@ public class Main {
 
         person2.showInfo();
 */
+/*
         Person p1 = new Person();
         p1.showFullInfo();
 
@@ -31,6 +34,21 @@ public class Main {
 
         Person p3 = new Person("Max", "Frai", 55, "Kyiv", 1980, 10,25);
         p3.showFullInfo();
+*/
+
+
+        Employee e1 = new Employee("Max1", "Frai1", 55, "Kyiv", 1980, 10,25);
+//        Employee e1 = ;
+//        Employee e2 = ;
+//        Employee e3 = ;
+        e1.showFullInfo();
+        List<Employee> employeeList = new ArrayList<Employee>();
+        employeeList.add(new Employee("Max1", "Frai1", 55, "Kyiv", 1980, 10,25));
+        employeeList.add(new Employee("Max2", "Frai2", 23, "Kyiv", 1999, 5,12));
+        employeeList.add(new Employee("Max3", "Frai3", 15, "Kyiv", 1945, 8,8));
+        for(Employee emp : employeeList){
+            emp.showFullInfo();
+        }
 
     }
 }
